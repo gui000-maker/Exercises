db9cc10aee357fe51c40f01cc77c27371e1f26e1
gui <gui151208@gmail.com>
2025-05-27 16:15:40 +0000

    added xor.c

--- xor.c
+++ xor.c
@@ -1,17 +1,32 @@
-#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define KEY '&'
 
-int main(void) {
-  int orig_char, new_char;
-  while ((orig_char = getchar()) != EOF) {
-    new_char = orig_char ^ KEY;
-    if (isprint(orig_char) && isprint(new_char))
-      putchar(new_char);
-    else
-      putchar(orig_char);
+int main(int argc, char *argv[]) {
+  FILE *fpin, *fpout;
+  char orig_char, new_char;
+
+  if (argc != 3) {
+    fprintf(stderr, "USAGE: xor input_filename output_filename\n");
+    exit(EXIT_FAILURE);
   }
 
-  return 0;
+  if ((fpin = fopen(argv[1], "rb")) == NULL) {
+    printf("Can't be created: %s\n", argv[1]);
+    exit(EXIT_FAILURE);
+  } else if ((fpout = fopen(argv[2], "wb")) == NULL) {
+    printf("Can't be created: %s\n", argv[2]);
+    exit(EXIT_FAILURE);
+  }
+
+  while ((orig_char = getc(fpin)) != EOF) {
+    new_char = orig_char ^ KEY;
+    putc(new_char, fpout);
+  }
+
+  fclose(fpin);
+  fclose(fpout);
+
+  exit(EXIT_SUCCESS);
 }
